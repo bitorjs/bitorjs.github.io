713a7e92d5c3313d2b760add8807da3b6d8c94ea
bitores <773155801@qq.com>
2019-04-27 16:19:59 +0800

    update

--- plugins/guide/src/view/guide/name.vue
+++ plugins/guide/src/view/guide/name.vue
@@ -4,7 +4,7 @@
     <ul>
       <li>component 命名优先级: name > filename</li>
       <li>filter 命名优先级: name > filename</li>
-      <li>controller 命名无要求</li>
+      <li>router 命名无要求</li>
       <li>view 命名无要求</li>
       <li>service 命名: filename</li>
       <li>mock 命名: filename, 与service 统一,方便切换</li>
--- plugins/guide/src/view/guide/plugin_dev.vue
+++ plugins/guide/src/view/guide/plugin_dev.vue
@@ -6,14 +6,37 @@
       // index.js
       
       export default (app, options) => {
-
         // app 应用实例
         // options 从主项目中传入的配置项目
-
       }
       </code>
     </pre>
-    <h3>插件中不需要实例化应用, 其它写法上与主项目写法一致</h3>
+    <p>插件中不需要实例化应用, 其它写法上与主项目写法一致</p>
+    <h2>开发步骤</h2>
+    <p>为了开发插件方便, 在主项目下创建一个目录, 如 plugins</p>
+    <p>创建插件如 xxx</p>
+    <ul>
+      <li>进入目录 plugins/xxx/</li>
+      <li>touch index.js 创建插件入口文件</li>
+      <li>npm init 创建 package.json</li>
+      <li>在入口文件 index.js 中添加上述代码</li>
+      <pre v-highlight>
+        <code class="js">
+        // index.js
+        export default (app, options) => {}
+        </code>
+      </pre>
+      <li>如需进行目录监听,可添加监听代码</li>
+      <pre v-highlight>
+        <code class="js">
+        // index.js
+        export default (app, options) => {
+          // 监听参数可自行修改
+          app.watch(require.context('./src', true, /^((?!\/view\/).)+\.(vue|js)$/));
+        }
+        </code>
+      </pre>
+    </ul>
   </div>
 </template>
 <script>
